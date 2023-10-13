01585e9c5986afe5e842ebdb6ffafd4b5fa7cdce
DuyNguyenProgrammer <hackerandanh2k3@gmail.com>
2023-10-13 22:23:57 +0700

    config axios and react-query

---
+++ src/utils/http.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios, { type AxiosInstance } from 'axios'
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'https://api-ecom.duthanhduoc.com/',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+const http = new Http().instance
+
+export default http
