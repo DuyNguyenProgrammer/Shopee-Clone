0176f30a873997027e6edc76d7527d20a66dd42c
DuyNguyenProgrammer <hackerandanh2k3@gmail.com>
2023-10-21 03:22:39 +0700

    setup protected route and rejected route in react-router-dom

--- src/pages/useRouteElements.tsx
+++ src/pages/useRouteElements.tsx
@@ -1,9 +1,20 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import ProductList from './ProductList'
 import Login from './Login'
 import Register from './Register'
 import RegisterLayout from 'src/layouts/RegisterLayout'
 import MainLayout from 'src/layouts/MainLayout'
+import Profile from './Profile'
+
+function ProtectedRoute() {
+  const isAuthenticated = true
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  const isAuthenticated = false
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
@@ -16,20 +27,40 @@ export default function useRouteElements() {
       )
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />{' '}
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />{' '}
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />{' '}
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />{' '}
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
   return routeElements
